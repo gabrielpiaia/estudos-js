ddc6464bdfcaec41486e7cbb42536d72c22d0032
unknown <gabrielpiaia95@gmail.com>
2024-02-14 15:32:42 -0300

    updates

---
+++ fundamentos/strings.js
@@ -0,0 +1,19 @@
+const nome = "G4br1el"
+
+console.log(nome.charAt(4))
+console.log(nome.charAt(3))
+
+console.log(nome.charCodeAt(3))
+
+
+console.log(nome.indexOf('3'))
+
+
+console.log(nome.substring(0, 4))
+
+console.log(nome.substring(2, 3))
+
+
+console.log('Meu nome é'.concat(nome).concat("!"))
+
+console.log(nome.replace(4, 'a'))
