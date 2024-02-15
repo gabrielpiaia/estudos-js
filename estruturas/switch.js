eafa292316db34024bb3f28a25aca4ab80d0dcbc
unknown <gabrielpiaia95@gmail.com>
2024-02-15 10:01:06 -0300

    switch e while

--- estruturas/switch.js
+++ estruturas/switch.js
@@ -1,3 +1,17 @@
-case imprimirResultado = function (nota) {
+const imprimirResultado = function (nota) {
+    switch (Math.floor(nota)) {
+        case 10:
+        case 9:
+            console.log('Excelente')
+           // break    //// ao entrar em um case vai executar todos até finalzar ou checar no break
+        case 8: case 7:
+            console.log('Aprovado')
+            break
+        default:
+            console.log('Nota inválida')
+    }
 
 }
+
+
+imprimirResultado(7)
