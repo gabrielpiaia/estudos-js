96f2a7dc87f275006c01be9ed431aad763ff1cb6
unknown <gabrielpiaia95@gmail.com>
2024-02-17 14:31:04 -0300

    update

---
+++ funcao/desafioFuncaoConstrutora.js
@@ -0,0 +1,27 @@
+/*
+class Pessoa {
+    constructor(nome) {
+        this.nome = nome
+    }
+
+    //metodo falar
+    falar() {
+        console.log('Meu nome é ', (this.nome))
+    }
+}
+
+
+const p1 = new Pessoa('Joao')
+p1.falar()
+*/
+
+function Pessoa(nome) {
+    this.nome = nome
+
+    this.falar = function() {
+        console.log(`Meu nome é ${this.nome}`)
+    }
+}
+
+const p1 = new Pessoa('teste')
+p1.falar()
---
+++ funcao/iife.js
@@ -0,0 +1,6 @@
+// IIFE expressão de função imediatamente invocada
+
+function() {
+    console.log('Será executado na hora!')
+    console.log('Foge do escopo mais abrangente')
+}()
