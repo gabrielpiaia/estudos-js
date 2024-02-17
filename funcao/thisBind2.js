706f6232fffcf7540b648f84b3d9aeec1e08dffa
unknown <gabrielpiaia95@gmail.com>
2024-02-17 12:09:52 -0300

    amarrar this na function com constante

---
+++ funcao/thisBind2.js
@@ -0,0 +1,15 @@
+function Pessoa() {
+    this.idade = 0
+//executar a cada 1 segundo a função anonima
+    const self = this  // usar self para amarrar o this na função
+    setInterval(function() {
+        self.idade++
+        console.log(self.idade)
+    }/*.bind(this)*/, 1000)   //quantidade de milisegundos
+}
+
+new Pessoa
+
+
+
+
